0242ea965c1af9ab3521915bbcb244af99056a56
Kirill <kirillko.lukyanov@gmail.com>
2019-08-03 13:32:00 +0300

    new ui for exudes list

--- EuropeanTravelPartner/ExcursionListViewController.swift
+++ EuropeanTravelPartner/ExcursionListViewController.swift
@@ -8,13 +8,28 @@
 
 import UIKit
 
-class ExcursionListViewController: UIViewController {
-
+class ExcursionListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    var placeList: [String] = ["CeskyKrumlov", "vienn"]
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return placeList.count
     }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! ImageTableViewCell
+        cell.excursLabel.text = placeList[indexPath.row]
+        cell.excursImage.image = UIImage(named: placeList[indexPath.row])
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 240
+    }
 
     @IBAction func kvExcursion(_ sender: Any) {
         performSegue(withIdentifier: "excBookingView", sender: self)
---
+++ EuropeanTravelPartner/ImageTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  ImageTableViewCell.swift
+//  EuropeanTravelPartner
+//
+//  Created by Кирилл Лукьянов on 03/08/2019.
+//  Copyright © 2019 Kirill Zaikin. All rights reserved.
+//
+
+import UIKit
+
+class ImageTableViewCell: UITableViewCell {
+    @IBOutlet weak var excursLabel: UILabel!
+    
+    @IBOutlet weak var excursImage: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
